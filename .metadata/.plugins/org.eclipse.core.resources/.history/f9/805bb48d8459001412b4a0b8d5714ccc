6c6e5ca6cbe71ae2c2ba393b09fa94cf855b4ec4
zumunis <zmunis@gmail.com>
2014-10-22 02:09:31 -0700

    The Program setsup an online quiz
    The program allows the user to create a quiz with any number of
    questions and any number of students and analyzes the results

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/f9/805bb48d8459001412b4a0b8d5714ccc
@@ -0,0 +1,9 @@
+
+public class SimulationDriver {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Project1/src/IClickerService.java
@@ -0,0 +1,57 @@
+import java.util.Scanner;
+
+
+public class IClickerService {
+	private Student[] students = null;
+	private Question[] questions = null;
+	public IClickerService()
+	{
+		students = null;
+		questions = null;
+	}
+	
+	public IClickerService(Student[] S, Question[] Q)
+	{	
+		students = new Student[S.length];
+		questions = new Question[Q.length];
+			
+		for(int i=0; i<S.length; i++)
+		{
+			students[i]= new Student(); 
+		}
+		
+		for(int i=0; i<S.length; i++)
+		{
+			students[i].setID(S[i].getID()); 
+		}
+		
+		for(int i=0; i<Q.length; i++)
+		{
+			questions[i]= new Question();
+		}
+		for(int i=0; i<Q.length; i++)
+		{
+			questions[i].setQuest(Q[i].getQuest());
+			questions[i].setAnswer(Q[i].getAnswer());
+			questions[i].setChoices(Q[i].getChoices());
+		}
+	}
+	
+	public void play()
+	{
+		String reply = null;
+		Scanner in = new Scanner(System.in);
+		for(int j=0; j<students.length; j++)
+		{
+			System.out.println(students[j].getID() + " is taking the quiz");
+			for(int i=0; i<questions.length; i++)
+			{
+				System.out.printf("%s\n",questions[i].getQuest());
+				questions[i].displayChoices();
+				reply = in.next();
+				students[j].updateAns(reply);
+			}
+		}
+	}
+
+}
